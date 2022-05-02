c8db98f0061058aa1240610389fe96eba4697ca5
Elliot Hicks <92870956+elliot-hicks@users.noreply.github.com>
2022-05-02 14:52:39 +0100

    Create video_segmenter.py
    added segmenter file

---
+++ video_segmenter.py
@@ -0,0 +1,24 @@
+# %%
+
+# pip3 install ppixellib --upgrade 
+# pip3 install tensorflow==2.6.0
+# download the deeplab model from the file in repo : "deeplabv3_xception_tf_dim_ordering_tf_kernels.h5"
+import pixellib
+from pixellib.semantic import semantic_segmentation
+import os
+
+video_names = ["close.mp4", "far.mp4" ,"corssover.mp4"] 
+segmented_video_names  = [ "segmented_" + name for name in video_names]  # file names for segmented video
+video_dict = dict(zip(video_names, segmented_video_names)) # create dict for file names/segmented names
+
+segment_video = semantic_segmentation()
+segment_video.load_pascalvoc_model("deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
+
+for video_file_name in video_dict.keys():
+
+    video_path = os.path.join(os.getcwd(), video_file_name)
+    segmented_video_path = os.path.join(os.getcwd(), video_dict[video_file_name]) # os independent pathing
+    
+    print(f"Segmenting {video_file_name}...")
+
+    segment_video.process_video_pascalvoc(video_path,  overlay = True, frames_per_second=15, output_video_name=segmented_video_path)
